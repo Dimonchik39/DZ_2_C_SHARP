f86de192841a82ac6cc58ad6d5142984d2a422f6
Dimonchik <sudjadred@live.com>
2022-09-06 16:47:01 +0200

    Task13 решено

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,18 +5,36 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+// Console.Clear();
+// Console.WriteLine("Введите целое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// string num = Convert.ToString(num1);
+
+// if (num1 > 99)
+// {
+//     if (num.Length > 2)
+//     Console.WriteLine($"Третья цифра из числа {num1}: " + num[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Введите число, не менее трехзначного");
+// }
+
 Console.Clear();
 Console.WriteLine("Введите целое число");
 int num1 = Convert.ToInt32(Console.ReadLine());
-string num = Convert.ToString(num1);
+int num2 = num1;
 
-if (num1 > 99)
+if (num1 < 100)
 {
-    if (num.Length > 2)
-    Console.WriteLine($"Третья цифра из числа {num1}: " + num[2]);
+    Console.WriteLine("Нет третьей цифры");
 }
 else
 {
-    Console.WriteLine("Введите число, не менее трехзначного");
+    while (num2 > 999)
+    {
+        num2 = num2 / 10;
+    }
+    num2 = (num2 % 10);
+    Console.WriteLine($"Третья цифра из числа {num1}: {num2}");
 }
-
