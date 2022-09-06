1a4c342ca32d08a4e0b558c2e0ce5787a7f25bb9
Dimonchik <sudjadred@live.com>
2022-09-06 21:11:17 +0200

    решили 15 задачку

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -3,3 +3,17 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5) Console.WriteLine("Рабочий день");
+
+else if (num == 6 || num == 7) Console.WriteLine("Ура - это выходной");
+
+else 
+{
+    Console.WriteLine("Еще не придумали такого дня");
+}
